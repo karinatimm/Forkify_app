c328cd28f5770361db4db6f935a2acba70d86bd2
Karina Timofeeva <kar.timofeeva93@gmail.com>
2024-02-22 11:45:10 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const TIMEOUT_SEC_FOR_LOADING = 1;
+export const RES_PER_PAGE = 10;
+export const KEY = '3e352851-0166-4f61-9071-e3ec442d3b97';
